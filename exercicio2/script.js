65363710c004352b644b8def52339372de6d1406
Gabriel Oliveira <gabrieloliveira.okada@gmail.com>
2022-07-19 01:06:08 -0300

    barbosaa-gabriel-oliveira

---
+++ exercicio2/script.js
@@ -0,0 +1,16 @@
+/*let pergunta1 = prompt("Você está com calor?")
+    let pergunta2 = prompt("Você gosta de cerveja?")
+        let pergunta3 = prompt("Você vai à igreja?")*/
+
+let pergunta1 = ("Você está com calor?")
+    let pergunta2 = ("Você gosta de cerveja?")
+        let pergunta3 = ("Você vai à igreja?")      
+
+let resposta1 = prompt(pergunta1)
+    let resposta2 = prompt(pergunta2)
+        let resposta3 = prompt(pergunta3)
+//console.log(resposta1, resposta2, resposta3)
+
+console.log(pergunta1, resposta1)
+console.log(pergunta2, resposta2)
+console.log(pergunta3, resposta3)
